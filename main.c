aecd0863f5149b690c0faa6858acaa8eabfd4906
Manuel Braga <mul.braga@gmail.com>
2013-11-12 18:39:22 +0000

    Parse long command line options.

--- main.c
+++ main.c
@@ -2,45 +2,85 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include "picture_t.h"
 #include "simplerecorder.h"
 
 #define mkv_filename "output.mkv"
 static struct picture_t pic;
+static char *option_input_filename  = NULL;
+static char *option_output_filename = NULL;
+static int option_width		= 0;
+static int option_height	= 0;
 
 static void usage(char *name)
 {
-	fprintf(stderr, "usage: %s width height [rawvideo.file]\n", name);
+	fprintf(stderr, "usage: %s [options] [rawfile]\n", name);
+	fprintf(stderr, "\t-o,\t-output=filename\n");
+	fprintf(stderr, "\t-w,\t-width\n");
+	fprintf(stderr, "\t-h,\t-height\n");
+	fprintf(stderr, "\n");
+}
+
+static struct option long_options[] = {
+	{"output",	required_argument,	0,	'o'},
+	{"width",	required_argument, 	0,	'w'},
+	{"height",	required_argument,	0, 	'h'},
+	{0, 0, 0, 0}
+};
+
+static void parse_options(int argc, char **argv)
+{
+	int c;
+	int index = 0;
+	while(1) {
+		c = getopt_long(argc, argv, "o:w:h:", long_options, &index);
+		if(c < 0)
+			break;
+
+		if(optarg && optarg[0] == '-')
+			c = -1;
+
+		switch(c) {
+			case 'o':
+				option_output_filename = optarg;
+				break;
+			case 'w':
+				option_width = atoi(optarg);
+				break;
+			case 'h':
+				option_height = atoi(optarg);
+				break;
+			default:
+				usage(argv[0]);
+				exit(1);
+				break;
+		}
+	}
+	if(optind < argc)
+		option_input_filename = argv[optind];
 }
 
 int main(int argc, char **argv)
 {
 	int i;
-	char *rawname = NULL;
 	struct encoded_pic_t encoded_pic,header_pic;
 	void *input_state;
 
-	if (argc != 3 && argc != 4) {
-		usage(argv[0]);
-		return 1;
-	}
+	parse_options(argc, argv);
+
 
 	memset(&pic, 0, sizeof(pic));
 
-	pic.width = atoi(argv[1]);
-	pic.height = atoi(argv[2]);
-	if (!pic.width || !pic.height) {
-		usage(argv[0]);
-		return 2;
-	}
-	if (argc == 4)
-		rawname = argv[3];
+	pic.width  = option_width;
+	pic.height = option_height;
 
-	if (0 > (input_state = input_init(rawname, &pic)))
+	if (0 > (input_state = input_init(option_input_filename, &pic)))
 		goto error_input;
 
-	fprintf(stderr, "rawvideo: %s\n", rawname);
+	fprintf(stderr, "rawvideo: %s\n", option_input_filename);
 	fprintf(stderr, "width: %d, height: %d\n", pic.width, pic.height);
+	fprintf(stderr, "output: %s\n", option_output_filename);
 
 	if(!encoder_init(&pic)){
 		fprintf(stderr,"failed to initialize encoder\n");
@@ -51,22 +91,27 @@ int main(int argc, char **argv)
 		goto error_input;
 	input_getframe(input_state, &pic);
 
-	if(!output_init(&pic,mkv_filename))
-		goto error_output;
+	if(option_output_filename)
+		if(!output_init(&pic, option_output_filename))
+			goto error_output;
+	
 	if(!encoder_encode_headers(&encoded_pic))
 		goto error_output;
-	memcpy(&header_pic,&encoded_pic,sizeof(encoded_pic));
-	header_pic.buffer=malloc(encoded_pic.length);
-	memcpy(header_pic.buffer,encoded_pic.buffer,encoded_pic.length);
-	if(!output_write_headers(&header_pic))
-		goto error_output;
+	
+	if(option_output_filename) {
+		memcpy(&header_pic,&encoded_pic,sizeof(encoded_pic));
+		header_pic.buffer=malloc(encoded_pic.length);
+		memcpy(header_pic.buffer,encoded_pic.buffer,encoded_pic.length);
 
-	// not sure about this -- but g_outputDataInfo.uSize0 appears to
-	// contain a whole frame at this point, so write it out and start
-	// at frame 1 in the loop
-	if(!output_write_frame(&encoded_pic))
-		goto error_encoder;
+		if(!output_write_headers(&header_pic))
+			goto error_output;
 
+		// not sure about this -- but g_outputDataInfo.uSize0 appears to
+		// contain a whole frame at this point, so write it out and start
+		// at frame 1 in the loop
+		if(!output_write_frame(&encoded_pic))
+			goto error_encoder;	
+	}
 	encoder_release(&encoded_pic);
 
 	for(i=1 ;; i++){
@@ -76,8 +121,12 @@ int main(int argc, char **argv)
 		pic.timestamp.tv_usec = (i * 40000) % 1000000;	// 25 FPS
 		if(!encoder_encode_frame(&pic, &encoded_pic))
 			goto error_encoder;
-		if(!output_write_frame(&encoded_pic))
-			goto error_encoder;
+
+		if(option_output_filename) {
+			if(!output_write_frame(&encoded_pic))
+				goto error_encoder;
+		}
+
 		encoder_release(&encoded_pic);
 	}
 
